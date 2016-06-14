adf6393f6dea872c04a788dd3cf948a9b798fd3b
JaeHo Song <thruthesky@gmail.com>
2016-06-15 02:04:25 +0900

    Fix: admin will not load payment module

--- allthegate/allthegate-javascript.php
+++ allthegate/allthegate-javascript.php
@@ -11,6 +11,8 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
  */
+
+if ( current_user_can('manage_options') ) return;
 ?>
 <!--script language=javascript src="http://www.allthegate.com/plugin/AGSWallet_utf8.js"></script-->
 <script language=javascript src="http://www.allthegate.com/plugin/AGSWallet.js"></script>
